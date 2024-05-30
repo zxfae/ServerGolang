ee7ed43ea0878b3bde02d4eac9f84c503738a30d
zxfae <lecro.philippe@icloud.com>
2024-05-31 00:06:41 +0200

    fix secureHeaders

--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"net/http"
 	"time"
-	"fmt"
 )
 
 /*
@@ -26,16 +25,22 @@ func ServerParameters(handler http.Handler) *http.Server {
 		// 1000000000000000000000 bits == 1 MB
 		// Return 431 statusCode
 		MaxHeaderBytes: 1 << 20,
-		Handler: secureHeaders(handler),
+		Handler:        secureHeaders(handler),
 	}
 }
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("X-Frame-Options", "DENY")
+		//Prevention attaque xss, ressources, scripts, objects de ce domaine
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none'; base-uri 'self'")
+		//w.Header().Set("X-Content-Type-Options", "nosniff")
+		//w.Header().Set("X-Frame-Options", "DENY")
+		//Instruct to the browser to block the page if XSS attack detected
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		//w.Header().Set("Referrer-Policy", "no-referrer")
+		//w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+		//Desactivate geolocalisation, microphone && camera
+		w.Header().Set("Permissions-Policy", "geolocation=(), microphone=(), camera=()")
 		next.ServeHTTP(w, r)
 	})
 }
